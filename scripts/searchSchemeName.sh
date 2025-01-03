1fa0c0c0760793086c40512a06882088fb8e04c9
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-01-03 11:15:30 +0100

    feat: adjust search scheme naming

--- scripts/searchSchemeName.sh
+++ scripts/searchSchemeName.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 
 declare -A order
 order=([backtracking]="\(\mathcal{S}_{bt, k}\)"
-      [optimum]="\(\mathcal{S}_{OSS}\)"
+      [optimum]="\(\mathcal{S}_{OSS, k}\)"
       [01*0]="\(\mathcal{S}_{01*0, k}\)"
       [01*0_opt]="\(\mathcal{S}_{01*0-opt, k}\)"
       [pigeon]="\(\mathcal{S}_{ph, k}\)"
@@ -13,9 +13,9 @@ order=([backtracking]="\(\mathcal{S}_{bt, k}\)"
       [h2-k1]="\(\mathcal{S}_{H, k, k+1}\)"
       [h2-k2]="\(\mathcal{S}_{H, k, k+2}\)"
       [h2-k3]="\(\mathcal{S}_{H, k, k+3}\)"
-      [kianfar]="\(\mathcal{S}_{kianfar}\)"
-      [kucherov-k1]="\(\mathcal{S}_{kuch, k+1}\)"
-      [kucherov-k2]="\(\mathcal{S}_{kuch, k+2}\)"
-      [hato]="\(\mathcal{S}_{hato}\)"
+      [kianfar]="\(\mathcal{S}_{kianfar, k}\)"
+      [kucherov-k1]="\(\mathcal{S}_{kuch, k, k+1}\)"
+      [kucherov-k2]="\(\mathcal{S}_{kuch, k, k+2}\)"
+      [hato]="\(\mathcal{S}_{hato, k}\)"
 )
 echo "${order[$1]}"
